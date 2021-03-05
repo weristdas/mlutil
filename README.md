d096a5b295f0b098f06e1895012fb8a8dc3316f8
weristdas <yanjie@ict.ac.cn>
2021-03-05 07:13:52 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Introduction
+Utility functions used in machine learning.
+
+Right now it is not a mature toolsuite, but personal use only.
+
+#Requirements
+numpy
+pandas
+pytorch
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='mlutil',
+   version='0.1',
+   description='Util for ML',
+   author='Neil Jie Yan',
+   author_email='yanjie@ict.ac.cn, jiey@msr',
+   packages=['mlutil'],
+   url="http://weristdas",
+   install_requires=['numpy', 'pandas'], #external dependent packages
+)
